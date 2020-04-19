557507e05d28874dc7ed802a362baca8a188ae64
divik09 <diviktwr4@gmail.com>
2020-04-19 14:15:32 +0530

    second commit

--- src/main/java/com/bean/UserDTO.java
+++ src/main/java/com/bean/UserDTO.java
@@ -1,5 +1,44 @@
 package com.bean;
 
 public class UserDTO {
+	
+	public int id;
+	public String firstName;
+	public String lastName;
+	public String email;
+	public String password;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
 
 }
