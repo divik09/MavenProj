9a87004c379634b1e0aebeb12482df098bd6f698
divik09 <diviktwr4@gmail.com>
2020-04-19 15:28:48 +0530

    fifth commited

--- src/main/java/com/model/UserModel.java
+++ src/main/java/com/model/UserModel.java
@@ -5,6 +5,9 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import com.bean.UserDTO;
 
@@ -33,25 +36,33 @@ public class UserModel {
 		     return pk + 1;
 	}
 	
-	public static UserDTO retrieve() throws Exception {
+	public static List<UserDTO> retrieve() throws Exception {
+		ArrayList<UserDTO> list = new ArrayList<UserDTO>();
 		
-		UserDTO dto = new UserDTO();
 	  PreparedStatement pre = con.prepareStatement("Select * from st_user");
 	    ResultSet rs =  pre.executeQuery();
 	    while(rs.next()) {
+	    	UserDTO dto = new UserDTO();
 	    	dto.setId(rs.getInt(1));
 	      dto.setEmail(	rs.getString(2));
 	      dto.setPassword(rs.getString(3));
 	      dto.setFirstName(rs.getString(4));
 	      dto.setLastName(rs.getString(5));
+	      
+	      list.add(dto);
 	    }
 	  
-	  return dto;
+	  return list;
 	}
 	public static void main(String[] args) throws Exception {
 	 	System.out.println("This is NextPK= "+nextPK());
-	   UserDTO dto = retrieve();
-	   System.out.println(dto.getId());
+	   List<UserDTO> list = retrieve();
+	           Iterator<UserDTO> itr=   list.iterator();
+	           while(itr.hasNext()) {
+	        	  UserDTO dto =  itr.next();
+	              System.out.println(dto.getFirstName());
+	           }
+	     
 	
 	}
 
